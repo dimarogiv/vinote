cf26b668cec4425dae4816d8fbed83063e6f2d51
Dima Rogiv <dirsednet.it@gmail.com>
2024-01-28 13:58:14 +0200

    added horizontal navigation feature

--- navigation.lua
+++ navigation.lua
@@ -49,3 +49,32 @@ go_back = function()
   write_log("go_back: " .. path)
   go_to_note(path)
 end
+
+horizontal_navigation = function(direction)
+  local handle = io.popen("find \"..\" -not -path \"../.*\" -mindepth 1 -maxdepth 1 -type d 2>/dev/null")
+  local list = {}
+  --for item in handle:read("*l") do
+  while true do
+    local item = handle:read("*l")
+    if item == nil then break end
+    table.insert(list, item)
+  end
+  local current_file = vim.fn.expand("%:p:h")
+  local itemn = 0
+  current_file_t = vim.fn.split(current_file, "/")
+  for n = 1, #list, 1 do
+    local list_t = vim.fn.split(list[n], "/")
+    if list_t[#list_t] == current_file_t[#current_file_t] then
+      itemn = n
+      break
+    end
+  end
+  --print(list[itemn])
+  --print(list[itemn % #list])
+  --print((itemn % (#list + 1)))
+  if direction == 1 then
+    wgo_to_note(list[(itemn % #list) + 1])
+  elseif direction == -1 then
+    wgo_to_note(list[((itemn -2) % #list) + 1])
+  end
+end
